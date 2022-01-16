cd31d159c35322a668914f32e04c0a3ddf14b412
risolij <risolij@gmail.com>
2022-01-16 11:24:09 -0700

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -30,65 +30,53 @@ impl Peel {
     }
 
     pub fn contents(&self) -> String {
-        match  std::fs::read_to_string(self.input.clone()) {
-            Ok(file) => {
-                match (self.nums, self.reverse) {
-                    (true, true) => {
-                        file
-                            .split('\n')
-                            .enumerate()
-                            .map(|(k, v)| format!("{}: {}", k, v))
-                            .collect::<Vec<String>>()
-                            .join("\n")
-                            .split('\n')
-                            .rev()
-                            .take(self.lines + 1)
-                            .collect::<Vec<&str>>()
-                            .join("\n")
-                            .split('\n')
-                            .rev()
-                            .collect::<Vec<&str>>()
-                            .join("\n")
-
-                    },
-                    (_, true)    => {
-                        file
-                            .split('\n')
-                            .rev()
-                            .take(self.lines + 1)
-                            .collect::<Vec<&str>>()
-                            .join("\n")
-                            .split('\n')
-                            .rev()
-                            .collect::<Vec<&str>>()
-                            .join("\n")
-                    },
-                    (true, _)    => {
-                        file
-                            .split('\n')
-                            .enumerate()
-                            .map(|(k, v)| format!("{}: {}", k, v))
-                            .collect::<Vec<String>>()
-                            .join("\n")
-                            .split('\n')
-                            .take(self.lines + 1)
-                            .collect::<Vec<&str>>()
-                            .join("\n")
-                    },
-                    (_, _)       => {
-                        file
-                            .split('\n')
-                            .take(self.lines)
-                            .collect::<Vec<&str>>()
-                            .join("\n")
-                    },
-
-                }
+        match std::fs::read_to_string(self.input.clone()) {
+            Ok(file) => match (self.nums, self.reverse) {
+                (true, true) => file
+                    .split('\n')
+                    .enumerate()
+                    .map(|(k, v)| format!("{}: {}", k, v))
+                    .collect::<Vec<String>>()
+                    .join("\n")
+                    .split('\n')
+                    .rev()
+                    .take(self.lines + 1)
+                    .collect::<Vec<&str>>()
+                    .join("\n")
+                    .split('\n')
+                    .rev()
+                    .collect::<Vec<&str>>()
+                    .join("\n"),
+                (_, true) => file
+                    .split('\n')
+                    .rev()
+                    .take(self.lines + 1)
+                    .collect::<Vec<&str>>()
+                    .join("\n")
+                    .split('\n')
+                    .rev()
+                    .collect::<Vec<&str>>()
+                    .join("\n"),
+                (true, _) => file
+                    .split('\n')
+                    .enumerate()
+                    .map(|(k, v)| format!("{}: {}", k, v))
+                    .collect::<Vec<String>>()
+                    .join("\n")
+                    .split('\n')
+                    .take(self.lines + 1)
+                    .collect::<Vec<&str>>()
+                    .join("\n"),
+                (_, _) => file
+                    .split('\n')
+                    .take(self.lines)
+                    .collect::<Vec<&str>>()
+                    .join("\n"),
             },
             Err(e) => {
                 println!("{}", e);
                 exit(1)
-            },
+            }
         }
     }
 }
